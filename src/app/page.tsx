af2ba61bb00272c20b3894d93c98ac451e4db873
Justin Morris <jnatmorris@gmail.com>
2023-07-30 13:46:43 +0200

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
     console.log("Landing page (server component)");
     return (
         <main>
+            <Link href="./page1">To page 1</Link>
             <h1 className="font-bold text-2xl text-blue-400">Landing page</h1>
         </main>
     );
